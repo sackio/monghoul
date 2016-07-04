da30d8db45a65e9a88647060362ccd55ed900d44
Ben Sack <ben@sack.io>
2016-07-04 11:55:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# monghoul
+
+mongro (Mongodb REST API) bindings for PhantomJS
+
+## License
+Copyright (c) 2016 Ben Sack  
+Licensed under the MIT license.
---
+++ monghoul.js
@@ -0,0 +1,103 @@
+/*
+  Framework and tools for Phantomjs
+
+  Webpage = require('webpage')
+  System = require('system')
+  Phantom = phantom
+  CP = require('child_process')
+  FS = require('fs')
+  Server = require('webserver')
+
+  requires:
+  _
+  Belt
+  Moment
+  Async
+*/
+
+var Monghoul = function(O){
+  var M = {};
+
+  O = O || {
+
+  };
+
+////////////////////////////////////////////////////////////////////////////////
+////METHODS                                                                 ////
+////////////////////////////////////////////////////////////////////////////////
+
+  M['_request'] = function(options, callback){
+    var a = Belt.argulint(arguments)
+      , self = this;
+    a.o = _.defaults(a.o, {
+      'page': Webpage.create()
+    , 'host': 'http://localhost:9354'
+    , 'url': _.template('<%= host %>/db/<%= db %>/collection/<%= collection %>/method/<%= method %>.json')
+      //db
+      //collection
+      //method
+      //data
+    });
+
+    return Async.waterfall([
+      function(cb){
+        return a.o.page.open(a.o.url(a.o), {
+          'operation': 'POST'
+        , 'encoding': 'utf8'
+        , 'headers': {
+            'Content-Type': 'application/json'
+          , 'Accept': 'application/json'
+          }
+        , 'data': Belt.stringify(a.o.data)
+        }, Belt.cw(cb, 0));
+      }
+    , function(cb){
+        try {
+          gb['data'] = Belt.parse(a.o.page.plainText);
+          return cb();
+        } catch(e) {
+          return cb(new Error('error retrieving data'));
+        }
+      }
+    ], function(err){
+      return a.cb(err, gb.response)
+    });
+  };
+
+  _.each([
+    'find'
+  , 'findOne'
+  , 'findOneAndUpdate'
+  , 'findOneAndReplace'
+  , 'findOneAndRemove'
+  , 'updateOne'
+  , 'updateMany'
+  , 'insertOne'
+  , 'insertMany'
+  , 'deleteOne'
+  , 'deleteMany'
+  ], function(v){
+    M[v] = function(options, callback){
+      var a = Belt.argulint(arguments)
+        , self = this;
+      a.o = _.defaults(a.o, {
+        //db
+        //collection
+        //more...
+      });
+
+      return self._request(_.extend({}, {
+        'method': v
+      , 'data': _.omit(a.o, [
+          'db'
+        , 'collection'
+        , 'host'
+        , 'url'
+        , 'page'
+        ])
+      }, a.o), a.cb);
+    });
+  });
+
+  return M;
+};
